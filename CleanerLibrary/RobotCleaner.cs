9d0605a84b88aa9caf4ee809c845c5f7e01d7ff4
Artem Osta <ostart@mail.ru>
2021-11-06 07:54:30 +0300

    Dependency Injection first approach

--- CleanerLibrary/RobotCleaner.cs
+++ CleanerLibrary/RobotCleaner.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace CleanerLibrary
 {
-    public class RobotCleaner
+    public class RobotCleaner: IRobotCleaner
     {
         private State _state {get;set;}
 
--- CleanerUsage/Program.cs
+++ CleanerUsage/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using CleanerLibrary;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace CleanerUsage
 {
@@ -7,10 +9,20 @@ namespace CleanerUsage
     {
         static void Main(string[] args)
         {
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((_, services) => {
+                    services.AddTransient<IRobotCleaner>(x => new RobotCleaner(TransferToCleaner));
+                })
+                .Build();
+            using IServiceScope serviceScope = host.Services.CreateScope();
+            IServiceProvider provider = serviceScope.ServiceProvider;
+            var robot = provider.GetRequiredService<IRobotCleaner>();
+
             Console.WriteLine("Hello World!");
-            var robot = new RobotCleaner(TransferToCleaner);
             robot.Work("inputCommands.txt");
             Console.WriteLine("Goodbuy World!");
+
+            host.Run();
         }
 
         private static void TransferToCleaner(string message)
