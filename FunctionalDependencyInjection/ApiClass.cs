576f6da341e8bef3794e46ee9dde35e79305cb0c
Artem Osta <ostart@mail.ru>
2021-11-10 07:10:17 +0300

    Functional DI first approach

---
+++ FunctionalDependencyInjection/ApiClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using StatelessCleanerLibrary;
+
+namespace FunctionalDependencyInjection
+{
+    public class ApiClass
+    {
+        private StatelessRobotCleaner _robot = new StatelessRobotCleaner();
+        private State _robotState = new State();
+
+        private void TransferToCleaner(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Move(int meters, State state, Action<string> transferDelegate, Action<int, State, Action<string>> moveFunction)
+        {
+            moveFunction(meters, state, transferDelegate);
+        }
+
+        public void Turn(int angleDegrees, State state, Action<string> transferDelegate, Action<int, State, Action<string>> turnFunction)
+        {
+            turnFunction(angleDegrees, state, transferDelegate);
+        }
+
+        public void Set(Tools tool, State state, Action<string> transferDelegate, Action<Tools, State, Action<string>> setFunction)
+        {
+            setFunction(tool, state, transferDelegate);
+        } 
+        
+        public void Start(State state, Action<string> transferDelegate, Action<State, Action<string>> startFunction)
+        {
+            startFunction(state, transferDelegate);
+        }
+        
+        public void Stop(Action<string> transferDelegate, Action<Action<string>> stopFunction)
+        {
+            stopFunction(transferDelegate);
+        }
+
+    }
+}
