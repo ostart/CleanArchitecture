62623c1c328b26feebee552c0d36e54e573eb95a
Artem Osta <ostart@mail.ru>
2021-11-18 06:42:49 +0300

    Monadic State First approach

---
+++ MonadicState/MonadicStateApi.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using StatelessCleanerLibrary;
+
+namespace MonadicState
+{
+    public class MonadicStateApi
+    {
+        private readonly StatelessRobotCleaner _robot;
+        private readonly Action<string> _transferToCleaner;
+
+        public MonadicStateApi(StatelessRobotCleaner robot, Action<string> transferToCleaner)
+        {
+            _robot = robot;
+            _transferToCleaner = transferToCleaner;
+        }
+
+        public State Move(int meters, State robotState)
+        {
+            _robot.Move(meters, robotState, _transferToCleaner);
+            return robotState;
+        }
+
+        public State Turn(int angleDegrees, State robotState)
+        {
+            _robot.Turn(angleDegrees, robotState, _transferToCleaner);
+            return robotState;
+        }
+
+        public State Set(Tools tool, State robotState)
+        {
+            _robot.Set(tool, robotState, _transferToCleaner);
+            return robotState;
+        } 
+        
+        public State Start(State robotState)
+        {
+            _robot.Start(robotState, _transferToCleaner);
+            return robotState;
+        }
+        
+        public State Stop(State robotState)
+        {
+            _robot.Stop(_transferToCleaner);
+            return robotState;
+        }
+    }
+}
